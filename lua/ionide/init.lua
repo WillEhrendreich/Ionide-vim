04ff0402a62d5edc694bd5ca7b2756d3f940e749
WillEhrendreich <will.ehrendreich@gmail.com>
2023-02-07 11:00:36 -0600

    finally selection working how I want this time.

--- lua/ionide/init.lua
+++ lua/ionide/init.lua
@@ -1649,100 +1649,96 @@ function M.ToggleFsi()
   end
 end
 
-local function get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
-  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-  else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-  end
-  return table.concat(lines, '\n')
-end
-
-function M.GetVisualSelection()
+function M.GetVisualSelection(keepSelectionIfNotInBlockMode, advanceCursorOneLine, debugNotify)
   local line_start, column_start
   local line_end, column_end
+  -- if debugNotify is true, use vim.notify to show debug info.
+  debugNotify = debugNotify or false
+  -- keep selection defaults to false, but if true the selection will
+  -- be reinstated after it's cleared to set '> and '<
+  -- only relevant in visual or visual line mode, block always keeps selection.
+  keepSelectionIfNotInBlockMode = keepSelectionIfNotInBlockMode or false
+  -- advance cursor one line defaults to true, but is turned off for
+  -- visual block mode regardless.
+  advanceCursorOneLine = (function()
+    if keepSelectionIfNotInBlockMode == true then
+      return false
+    else
+      return advanceCursorOneLine or true
+    end
+  end)()
+
   if vim.fn.visualmode() == "\22" then
-    -- if vim.fn.visualmode() == "v" or vim.fn.visualmode() == "V" then
-    -- line_start, column_start = unpack(vim.fn.getpos("v"), 2)
-    -- line_end, column_end = unpack(vim.fn.getpos("."), 2)
+    line_start, column_start = unpack(vim.fn.getpos("v"), 2)
+    line_end, column_end = unpack(vim.fn.getpos("."), 2)
   else
-  -- does not handle rectangular selection
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
-  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-  local lines = vim.api.buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
+    -- if not in visual block mode then i want to escape to normal mode.
+    -- if this isn't done here, then the '< and '> do not get set,
+    -- and the selection will only be whatever was LAST selected.
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'x', true)
+    line_start, column_start = unpack(vim.fn.getpos("'<"), 2)
+    line_end, column_end = unpack(vim.fn.getpos("'>"), 2)
+  end
+  if (column_start) > (column_end) then
+    column_start, column_end = column_end, column_start
+    if debugNotify == true then
+      vim.notify("switching column start and end, \nWas " ..
+        column_end .. "," .. column_start .. "\nNow " .. column_start .. "," .. column_end)
+    end
+  end
+  if (line_start) > (line_end) then
+    line_start, line_end = line_end, line_start
+    if debugNotify == true then
+      vim.notify("switching line start and end, \nWas " ..
+        line_end .. "," .. line_start .. "\nNow " .. line_start .. "," .. line_end)
+    end
+  end
+  if vim.g.selection == "exclusive" then
+    column_end = column_end - 1 -- Needed to remove the last character to make it match the visual selection
+  end
+  if debugNotify == true then
+    vim.notify("vim.fn.visualmode(): " ..
+      vim.fn.visualmode() ..
+      "\nsel start " ..
+      vim.inspect(line_start) ..
+      " " .. vim.inspect(column_start) .. "\nSel end " .. vim.inspect(line_end) .. " " .. vim.inspect(column_end))
+  end
+  local n_lines = math.abs(line_end - line_start) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
   if #lines == 0 then
     return { "" }
   end
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  if vim.fn.visualmode() == "\22" then
+    -- this is what actually sets the lines to only what is found between start and end columns
+    for i = 1, #lines do
+      lines[i] = string.sub(lines[i], column_start, column_end)
+    end
   else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+    lines[1] = string.sub(lines[1], column_start, -1)
+    if n_lines == 1 then
+      lines[n_lines] = string.sub(lines[n_lines], 1, column_end - column_start + 1)
+    else
+      lines[n_lines] = string.sub(lines[n_lines], 1, column_end)
+    end
+    -- if advanceCursorOneLine == true, then i do want the cursor to advance once.
+    if advanceCursorOneLine == true then
+      if debugNotify == true then
+        vim.notify("advancing cursor one line past the end of the selection to line " .. vim.inspect(line_end + 1))
+      end
+      vim.api.nvim_win_set_cursor(0, { line_end + 1, 0 })
+    end
+
+    if keepSelectionIfNotInBlockMode then
+      vim.api.nvim_feedkeys("gv", "n", true)
+    end
   end
-  --   line_start, column_start = unpack(vim.fn.getpos("'<"), 2)
-  --   line_end, column_end = unpack(vim.fn.getpos("'>"), 2)
-  -- end
-  -- if (vim.fn.line2byte(line_start) + column_start) > (vim.fn.line2byte(line_end) + column_end) then
-  --   line_start, column_start, line_end, column_end = line_end, column_end, line_start, column_start
-  -- end
-  -- local lines = vim.fn.getline(line_start, line_end + 1)
-  -- if #lines == 0 then
-  --   return { "" }
-  -- end
-  -- if vim.g.selection == "exclusive" then
-  --   column_end = column_end - 1 -- Needed to remove the last character to make it match the visual selection
-  -- end
-    
-  -- if vim.fn.visualmode() == "\22" then
-  --   for i = 1, #lines do
-  --     lines[i] = string.sub(lines[i], column_start, column_end - 1)
-  --   end
-  -- else
-  --   lines[#lines] = string.sub(lines[#lines], column_start, column_end - 1)
-  --   lines[1] = string.sub(lines[1], column_start)
-  -- end
-
+  if debugNotify == true then
+    vim.notify(table.concat(lines, "\n"))
+  end
   return lines -- use this return if you want an array of text lines
-
   -- return table.concat(lines, "\n") -- use this return instead if you need a text block
 end
 
--- function M.GetVisualSelection()
---   -- vim.notify("getting visual selection")
---   -- vim.api.nvim_feedkeys("j", "n", false)
---   local line_start, column_start = unpack(vim.fn.getpos("'<"), 2)
---   -- local column_start = vim.fn.getpos("'<")[3]
---   -- vim.notify("line start: " .. line_start .. " column_start:" .. column_start)
---   local line_end, column_end = unpack(vim.fn.getpos("'>"), 2)
---   -- vim.notify("line end: " .. line_end .. " column_end:" .. column_end)
---   local lines = vim.fn.getline(line_start, line_end)
---   -- vim.notify("number of lines: " .. vim.inspect(#lines))
---   local len = #lines
---   if len == 0 then
---     return {}
---   end
---
---   local inclusive = vim.o.selection == "inclusive"
---   local columnSelectionSubraction = (function()
---
---     if inclusive then
---       return 1
---     else
---       return 2
---     end
---   end)()
---   lines[len] = string.sub(lines[len], 0, column_end - columnSelectionSubraction)
---   lines[1] = string.sub(lines[1], column_start)
---   -- vim.notify("lines: \n" .. vim.inspect(lines))
---   return lines
--- end
---
 --"
 --" function! fsharp#quitFsi()
 --"     if s:fsi_buffer >= 0 && bufexists(str2nr(s:fsi_buffer))
