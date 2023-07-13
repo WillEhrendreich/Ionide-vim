4e540ed7a9f0ee5884fad28e024a3a4ac54f7f3f
WillEhrendreich <will.ehrendreich@gmail.com>
2023-07-13 10:30:11 -0500

    chore: formatting of settingsPlugin registrations code

--- lua/ionide/init.lua
+++ lua/ionide/init.lua
@@ -26,25 +26,22 @@ local neotree = tryRequire("neotree.nvim")
 
 local neoconf = tryRequire("neoconf.plugins")
 if neoconf == nil then
-    ---@class SettingsPlugin
-    ---@field name string
-    ---@field setup fun()|nil
-    ---@field on_update fun(event)|nil
-    ---@field on_schema fun(schema: Schema)
+  ---@class SettingsPlugin
+  ---@field name string
+  ---@field setup fun()|nil
+  ---@field on_update fun(event)|nil
+  ---@field on_schema fun(schema: Schema)
   --construct a fake one to "register" the schema without it giving an error.
-    ---@type  SettingsPlugin
-  local fakeNeoConfRegisterOpts={
-  on_schema = function(schema)
+  ---@type  SettingsPlugin
+  local fakeNeoConfRegisterOpts = {
+    on_schema = function(schema) end,
+  }
 
-
-    end}
-
-  neoconf ={
+  neoconf = {
     ---comment
     ---@param plugin SettingsPlugin
-    register=function(plugin)
-
-  end}
+    register = function(plugin) end,
+  }
 end
 
 ---@class PackageReference
