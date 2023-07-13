64c976c2d9d279cc3659e05830b8bf29f213c595
WillEhrendreich <will.ehrendreich@gmail.com>
2023-07-13 10:46:54 -0500

    feat: added settings.FSharp.fsac.sourceTextImplementation default to RosylnSourceText

--- lua/ionide/init.lua
+++ lua/ionide/init.lua
@@ -386,6 +386,15 @@ M.DefaultServerSettings = {
     conserveMemory = true,
     silencedLogs = {},
     -- parallelReferenceResolution = true,
+    -- "FSharp.fsac.sourceTextImplementation": {
+    --        "default": "NamedText",
+    --    "description": "EXPERIMENTAL. Enables the use of a new source text implementation. This may have better memory characteristics. Requires restart.",
+    --      "enum": [
+    --        "NamedText",
+    --        "RoslynSourceText"
+    --      ]
+    --    },
+    sourceTextImplementation = "RoslynSourceText",
     dotnetArgs = {},
     -- netCoreDllPath = "",
   },
